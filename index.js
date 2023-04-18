a0d746c5fe7c08c1d09f7b42655d28b3639a0f8e
Zach Ruiz <powersbooth10@gmail.com>
2023-04-18 16:25:24 -0500

    install some node packages that produce a random super hero name and villain name.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-console.log("hello world!");
+// console.log("hello world!");
 
 
-//jshhint esversion:6
+// //jshhint esversion:6
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt")
+// fs.copyFileSync("file1.txt", "file2.txt")
+
+const superheroes = require("superheroes");
+
+const supervillains = require("supervillains");
+
+
+let mySupeheroName = superheroes.random();
+
+let mySuperVillainName = supervillains.random();
+
+console.log("Your super villain name is: " + mySuperVillainName);
+
+console.log("Your super hero name is: " + mySupeheroName);
